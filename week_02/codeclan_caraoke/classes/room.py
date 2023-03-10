af4b40d3de06c1292e377756e28e2535bd0bdff2
Sarah Smith <sarahsmithvii@gmail.com>
2023-03-10 15:21:49 +0000

    add guest test

--- week_02/codeclan_caraoke/classes/room.py
+++ week_02/codeclan_caraoke/classes/room.py
@@ -1,3 +1,11 @@
 class Room:
     def __init__(self, name):
         self.name = name
+        self.guest_list = []
+
+    def guest_count(self):
+        return len(self.guest_list)
+    
+    def add_guest(self, guest):
+        self.guest_list.append(guest)
+    
--- week_02/codeclan_caraoke/tests/room_test.py
+++ week_02/codeclan_caraoke/tests/room_test.py
@@ -1,9 +1,19 @@
 import unittest
 from classes.room import *
+from classes.guest import *
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room = Room("Mercury Room")
+        self.guest = Guest("Michael Jackson")
 
     def test_room_has_name(self):
         self.assertEqual("Mercury Room", self.room.name)
+
+    def test_guests_in_room_empty(self):
+        self.assertEqual(0, self.room.guest_count())
+
+    def test_one_guest_in_room(self):
+        self.room.add_guest(self.guest)
+        self.assertEqual(1, self.room.guest_count())
+    
