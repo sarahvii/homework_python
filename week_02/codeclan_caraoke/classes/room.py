2df218ea4dd7299102e0860713e838a9650779f2
Sarah Smith <sarahsmithvii@gmail.com>
2023-03-10 16:28:45 +0000

    test add remove song

--- week_02/codeclan_caraoke/classes/room.py
+++ week_02/codeclan_caraoke/classes/room.py
@@ -2,6 +2,7 @@ class Room:
     def __init__(self, name):
         self.name = name
         self.guest_list = []
+        self.playlist = []
 
     def guest_count(self):
         return len(self.guest_list)
@@ -11,3 +12,12 @@ class Room:
     
     def check_out_guest(self, guest):
         self.guest_list.remove(guest)
+
+    def song_count(self):
+        return len(self.playlist)
+    
+    def add_song(self, song):
+        self.playlist.append(song)
+
+    def remove_song(self, song):
+        self.playlist.remove(song)
--- week_02/codeclan_caraoke/tests/room_test.py
+++ week_02/codeclan_caraoke/tests/room_test.py
@@ -7,6 +7,7 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room = Room("Mercury Room")
         self.guest = Guest("Michael Jackson")
+        self.song = Song("Final Countdown")
 
     def test_room_has_name(self):
         self.assertEqual("Mercury Room", self.room.name)
@@ -22,6 +23,19 @@ class TestRoom(unittest.TestCase):
         self.room.check_in_guest(self.guest)
         self.room.check_out_guest(self.guest)
         self.assertEqual(0, self.room.guest_count())
+
+    def test_playlist_is_empty(self):
+        self.assertEqual(0, self.room.song_count())
+
+    def test_can_add_song(self):
+        self.room.add_song(self.song)
+        self.assertEqual(1, self.room.song_count())
+
+    def test_can_remove_song(self):
+        self.room.add_song(self.song)
+        self.room.remove_song(self.song)
+        self.assertEqual(0, self.room.song_count())
+
 
         
 
